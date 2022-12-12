f450579d47087783b327919e72ffce93ed78ed4c
donatomontrone <donato.montrone@outlook.com>
2022-12-12 15:52:04 +0100

    added img

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            message: 'Hello Vue!'
+            message: 'Hello Vue!',
+            urlImage: 'https://picsum.photos/300/200?random=1'
         }
     }
 }).mount('#app')
